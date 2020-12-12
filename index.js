e0c1ad6f36482dc178e29b9625e2aff9a8bd9143
Dan Leaver <dleaver@gmail.com>
2020-12-11 19:07:26 -0700

    got it working

--- index.js
+++ index.js
@@ -2,32 +2,65 @@ let leftSide = ""
 let rightSide = ""
 let operator = ""
 
+let leftsideSpan = document.getElementById("leftside")
+let rightsideSpan = document.getElementById("rightside")
+let operatorSpan = document.getElementById("operator")
+let answerSpan = document.getElementById("answer")
+
 handleClick = (num) => {
   if (operator === "") {
     leftSide += num
-    document.getElementById("leftside").innerHTML = leftSide
+    leftsideSpan.innerHTML = leftSide
   } else {
     rightSide += num
-    document.getElementById("rightside").innerHTML = rightSide
+    rightsideSpan.innerHTML = rightSide
   }
 }
 
 chooseOperator = (incomingOperator) => {
+  if (leftSide === "") return;
+
   operator = incomingOperator
-  document.getElementById("operator").innerHTML = operator
+  operatorSpan.innerHTML = operator
 }
 
 evaluateAnswer = () => {
+  if (rightSide == "") return;
+
   let answer = ""
+  let leftSideInt = parseFloat(leftSide)
+  let rightSideInt = parseFloat(rightSide)
+
   switch (operator) {
-    case "+":
-      answer = leftSide + rightSide
-      break;
-    case "-":
-      break;
-    case "*":
-      break;
+  case "+":
+    answer = leftSideInt + rightSideInt
+    break;
+  case "-":
+    answer = leftSideInt - rightSideInt
+    break;
+  case "*":
+    answer = leftSideInt * rightSideInt
+    break;
+  case "/":
+    answer = leftSideInt / rightSideInt
+    break;
   }
 
-  document.getElementById("answer").innerHTML = answer
+  console.log("answer: ", answer)
+
+  if (answer === Infinity) {
+    answerSpan.innerHTML = "Error, cannot divide by zero!"
+  } else {
+    answerSpan.innerHTML = answer
+  }
+  
+  answerSpan.innerHTML += `&#x1F970`
+
+  leftsideSpan.innerHTML = ""
+  rightsideSpan.innerHTML = ""
+  operatorSpan.innerHTML = ""
+
+  operator = ""
+  leftSide = ""
+  rightSide = ""
 }
